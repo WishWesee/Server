a514480de6932c82ea7662f5ffe6a4e78c5311cf
이지수 <147130052+leejisooo@users.noreply.github.com>
2024-12-21 22:41:41 +0900

    Initial commit

---
+++ src/main/java/depth/main/wishwesee/WishweseeApplication.java
@@ -0,0 +1,13 @@
+package depth.main.wishwesee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WishweseeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WishweseeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/depth/main/wishwesee/WishweseeApplicationTests.java
@@ -0,0 +1,13 @@
+package depth.main.wishwesee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WishweseeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
