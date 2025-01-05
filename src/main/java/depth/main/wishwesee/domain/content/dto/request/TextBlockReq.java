d584be294358ae4a6ed0d89a46ec4d0f75aeae22
LeeJiSoo <ljs08010@gmail.com>
2025-01-06 04:25:21 +0900

    [DOCS]스웨거 명세 추가

--- src/main/java/depth/main/wishwesee/domain/content/dto/request/TextBlockReq.java
+++ src/main/java/depth/main/wishwesee/domain/content/dto/request/TextBlockReq.java
@@ -1,9 +1,12 @@
 package depth.main.wishwesee.domain.content.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 @Getter
+@Schema(description = "텍스트 블록 요청")
 public class TextBlockReq extends BlockReq{
+    @Schema(description = "텍스트 내용", type = "string")
     private String content;
 
 }
