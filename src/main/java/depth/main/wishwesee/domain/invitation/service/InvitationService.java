8e1b1e1f191289badba2ec7a411d1ff73d4ce92c
LeeJiSoo <ljs08010@gmail.com>
2025-01-08 23:13:44 +0900

    [FEAT] ResponseEntity 응답 추가

--- src/main/java/depth/main/wishwesee/domain/invitation/service/InvitationService.java
+++ src/main/java/depth/main/wishwesee/domain/invitation/service/InvitationService.java
@@ -9,7 +9,9 @@ import depth.main.wishwesee.domain.user.domain.repository.UserRepository;
 import depth.main.wishwesee.global.config.security.token.UserPrincipal;
 import depth.main.wishwesee.global.exception.DefaultException;
 import depth.main.wishwesee.global.payload.ErrorCode;
+import depth.main.wishwesee.global.payload.ErrorResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -33,15 +35,18 @@ public class InvitationService {
 
         // 작성자 본인 확인
         if(invitation.getSender().getId().equals(receiver.getId())){
-            throw new DefaultException(ErrorCode.INVALID_PARAMETER, "본인이 작성한 초대장은 저장할 수 없습니다.");
+            ErrorResponse errorResponse = ErrorResponse.of(ErrorCode.INVALID_PARAMETER, "본인이 작성한 초대장은 저장할 수 없습니다.");
+            return ResponseEntity.badRequest().body(errorResponse);
         }
 
-        // 중복 확인 (초대장이 이미 저장된 경우 예외 발생)
+        // 중복 확인 (초대장이 이미 저장된 경우)
         boolean alreadyExists = receivedInvitationRepository.existsByReceiverAndInvitation(receiver, invitation);
         if (alreadyExists) {
-            throw new DefaultException(ErrorCode.DUPLICATE_ERROR, "이미 내 목록에 저장된 초대장입니다.");
+            ErrorResponse errorResponse = ErrorResponse.of(ErrorCode.DUPLICATE_ERROR, "이미 내 목록에 저장된 초대장입니다.");
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(errorResponse);
         }
 
+        // 초대장 저장
         ReceivedInvitation receivedInvitation = ReceivedInvitation.builder()
                 .receiver(receiver)
                 .invitation(invitation)
