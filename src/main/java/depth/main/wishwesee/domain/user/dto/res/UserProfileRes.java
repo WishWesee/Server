7947d7876e9166b62fc8efe9ceedefe9f2d99420
jisu <98259433+jisu-jeong0@users.noreply.github.com>
2025-01-09 07:34:08 +0900

    [FEAT] 계정명, 프로필 조회 구현

---
+++ src/main/java/depth/main/wishwesee/domain/user/dto/res/UserProfileRes.java
@@ -0,0 +1,21 @@
+package depth.main.wishwesee.domain.user.dto.res;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserProfileRes {
+
+    @Schema(type = "Long", example = "1", description = "사용자의 userId입니다.")
+    private Long userId;
+
+    @Schema(type = "String", example = "김뎁스", description = "사용자의 이름입니다.")
+    private String name;
+
+    @Schema(type = "String", example = "https://lh3.googleusercontent.com/a/ACg8ocLU6k_7elIaIDZKUmqVPU3bg-Eajwu-gV", description = "사용자의 프로필 사진입니다.")
+    private String image;
+}
