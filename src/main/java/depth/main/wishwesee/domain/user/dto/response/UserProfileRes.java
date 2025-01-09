60d3429748e55daa220fe03898c3af2b30d2e549
jisu <98259433+jisu-jeong0@users.noreply.github.com>
2025-01-09 21:20:59 +0900

    [FIX] 프로필 조회 오류 수정

--- src/main/java/depth/main/wishwesee/domain/user/dto/response/UserProfileRes.java
+++ src/main/java/depth/main/wishwesee/domain/user/dto/response/UserProfileRes.java
@@ -3,8 +3,10 @@ package depth.main.wishwesee.domain.user.dto.response;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
