407006a3bbd198ef66068373be26801c47b7c145
LeeJiSoo <ljs08010@gmail.com>
2025-01-14 15:30:05 +0900

    [FIX] conflict해결

--- src/main/java/depth/main/wishwesee/domain/invitation/controller/InvitationController.java
+++ src/main/java/depth/main/wishwesee/domain/invitation/controller/InvitationController.java
@@ -20,13 +20,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.Optional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
+
 
 import java.util.List;
 
