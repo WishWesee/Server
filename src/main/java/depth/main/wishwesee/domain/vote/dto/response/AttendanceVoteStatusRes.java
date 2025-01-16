06e2857d775934af6395eaa302f62c96f46efcba
jisu <98259433+jisu-jeong0@users.noreply.github.com>
2025-01-16 20:45:26 +0900

    [DOCS] response에 스웨거 명세 추가

--- src/main/java/depth/main/wishwesee/domain/vote/dto/response/AttendanceVoteStatusRes.java
+++ src/main/java/depth/main/wishwesee/domain/vote/dto/response/AttendanceVoteStatusRes.java
@@ -1,5 +1,6 @@
 package depth.main.wishwesee.domain.vote.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -11,7 +12,9 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class AttendanceVoteStatusRes {
 
+    @Schema(type = "int", example = "4", description = "참석에 투표한 투표자들의 수입니다.")
     private int attending;
 
+    @Schema(type = "int", example = "2", description = "불참에 투표한 투표자들의 수입니다.")
     private int notAttending;
 }
--- src/main/java/depth/main/wishwesee/domain/vote/dto/response/CheckNicknameRes.java
+++ src/main/java/depth/main/wishwesee/domain/vote/dto/response/CheckNicknameRes.java
@@ -1,5 +1,6 @@
 package depth.main.wishwesee.domain.vote.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -11,5 +12,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class CheckNicknameRes {
 
+    @Schema(type = "boolean", example = "true", description = "닉네임의 중복 여부입니다. true: 이미 존재함, false: 존재하지 않음")
     private boolean isDuplicated;
 }
