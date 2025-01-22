694e3f4338b1e7ab4c4cd607e2376893d357d69c
jisu <98259433+jisu-jeong0@users.noreply.github.com>
2025-01-22 20:58:04 +0900

    [REFACTOR] 테이블명 변경에 따른 repository명 변경

--- src/main/java/depth/main/wishwesee/domain/vote/domain/repository/ScheduleVoterRepository.java
+++ src/main/java/depth/main/wishwesee/domain/vote/domain/repository/ScheduleVoterRepository.java
@@ -3,5 +3,5 @@ package depth.main.wishwesee.domain.vote.domain.repository;
 import depth.main.wishwesee.domain.vote.domain.ScheduleVoter;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface VoterNicknameRepository extends JpaRepository<ScheduleVoter, Long> {
+public interface ScheduleVoterRepository extends JpaRepository<ScheduleVoter, Long> {
 }
