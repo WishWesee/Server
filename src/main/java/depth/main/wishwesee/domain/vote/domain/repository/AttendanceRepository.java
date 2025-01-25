e9f8cf16ab9a95adb8fe020f0ad19cad8ceaaac7
jisu <98259433+jisu-jeong0@users.noreply.github.com>
2025-01-25 18:56:58 +0900

    [REFACTOR] `@Repository` 일괄 추가

--- src/main/java/depth/main/wishwesee/domain/vote/domain/repository/AttendanceRepository.java
+++ src/main/java/depth/main/wishwesee/domain/vote/domain/repository/AttendanceRepository.java
@@ -6,10 +6,12 @@ import depth.main.wishwesee.domain.vote.domain.Attendance;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface AttendanceRepository extends JpaRepository<Attendance, Long> {
     Optional<Attendance> findByInvitationAndUser(Invitation invitation, User user);
 
