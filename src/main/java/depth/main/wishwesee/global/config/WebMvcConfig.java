51a55158697ac60220da5bc64213abf1496aa1ea
LeeJiSoo <ljs08010@gmail.com>
2025-01-31 02:35:05 +0900

    [CHORE] CORS 설정 로직 개선

--- src/main/java/depth/main/wishwesee/global/config/WebMvcConfig.java
+++ src/main/java/depth/main/wishwesee/global/config/WebMvcConfig.java
@@ -1,24 +1,30 @@
 package depth.main.wishwesee.global.config;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
 @Configuration
 @EnableWebMvc
+@ConfigurationProperties(prefix = "app.cors")
 public class WebMvcConfig implements WebMvcConfigurer {
 
     private final long MAX_AGE_SECS = 3600;
 
-    @Value("${app.cors.allowed-origins}")
-    private String[] allowedOrigins;
+    private List<String> allowedOrigins;
+
+    public void setAllowedOrigins(List<String> allowedOrigins) {
+        this.allowedOrigins = allowedOrigins;
+    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(allowedOrigins)
+                .allowedOrigins(allowedOrigins.toArray(new String[0])) // 리스트를 배열로 변환하여 적용
                 .allowedOriginPatterns()
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
